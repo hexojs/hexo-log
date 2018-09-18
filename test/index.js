4403a38d0a44379923e2f3564fbd11a2031a6399
YoshinoriN <nayo-msoft@outlook.jp>
2018-09-18 20:57:29 +0900

    Migrate to ES6

--- test/index.js
+++ test/index.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var should = require('chai').should(); // eslint-disable-line
-var rewire = require('rewire');
-var sinon = require('sinon');
+const should = require('chai').should(); // eslint-disable-line
+const rewire = require('rewire');
+const sinon = require('sinon');
 
-describe('hexo-log', function() {
-  var logger = require('../lib/log');
-  var loggerModule = rewire('../lib/log');
+describe('hexo-log', () => {
+  const logger = require('../lib/log');
+  const loggerModule = rewire('../lib/log');
 
-  it('add alias for levels', function() {
-    var log = logger();
+  it('add alias for levels', () => {
+    let log = logger();
 
     log.d.should.eql(log.debug);
     log.i.should.eql(log.info);
@@ -18,39 +18,39 @@ describe('hexo-log', function() {
     log.log.should.eql(log.info);
   });
 
-  it('default name is hexo', function() {
-    var log = logger();
+  it('default name is hexo', () => {
+    let log = logger();
 
     log.fields.name.should.eql('hexo');
   });
 
-  it('options.name', function() {
-    var log = logger({ name: 'foo' });
+  it('options.name', () => {
+    let log = logger({ name: 'foo' });
 
     log.fields.name.should.eql('foo');
   });
 
-  it('level should be trace if options.debug is true', function() {
-    var log = logger({ debug: true });
+  it('level should be trace if options.debug is true', () => {
+    let log = logger({ debug: true });
 
     log.streams[0].level.should.eql(10);
   });
 
-  it('should add file stream if options.debug is true', function() {
-    var log = logger({ debug: true });
+  it('should add file stream if options.debug is true', () => {
+    let log = logger({ debug: true });
 
     log.streams[1].path.should.eql('debug.log');
   });
 
-  it('should remove console stream if options.silent is true', function() {
-    var log = logger({ silent: true });
+  it('should remove console stream if options.silent is true', () => {
+    let log = logger({ silent: true });
 
     log.streams.length.should.eql(0);
   });
 
-  it('should display time if options.debug is true', function() {
-    var spy = sinon.spy();
-    var now = new Date();
+  it('should display time if options.debug is true', () => {
+    const spy = sinon.spy();
+    const now = new Date();
 
     loggerModule.__with__({
       process: {
@@ -58,9 +58,9 @@ describe('hexo-log', function() {
           write: spy
         }
       }
-    })(function() {
+    })(() => {
       sinon.useFakeTimers(now.valueOf());
-      var log = loggerModule({ debug: true });
+      const log = loggerModule({ debug: true });
       log.info('test');
       sinon.restore();
     });
@@ -68,8 +68,8 @@ describe('hexo-log', function() {
     spy.args[0][0].should.contain(now.toISOString().substring(11, 23));
   });
 
-  it('should print error to process.stderr stream', function() {
-    var spy = sinon.spy();
+  it('should print error to process.stderr stream', () => {
+    const spy = sinon.spy();
 
     loggerModule.__with__({
       process: {
@@ -77,8 +77,8 @@ describe('hexo-log', function() {
           write: spy
         }
       }
-    })(function() {
-      var log = loggerModule();
+    })(() => {
+      const log = loggerModule();
       log.error('test');
     });
 
