293dcb1b4db69c69ea90200788bd3012fac11d17
SukkaW <isukkaw@gmail.com>
2020-04-25 21:27:39 +0800

    refactor: completely rewrite the logger

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [![NPM version](https://badge.fury.io/js/hexo-log.svg)](https://www.npmjs.com/package/hexo-log)
 [![Coverage Status](https://coveralls.io/repos/hexojs/hexo-log/badge.svg?branch=master)](https://coveralls.io/r/hexojs/hexo-log?branch=master)
 
-Logger for Hexo, based on [bunyan] with better console output.
+Logger for Hexo.
 
 ## Installation
 
@@ -15,10 +15,10 @@ $ npm install hexo-log --save
 ## Usage
 
 ``` js
-var log = require('hexo-log')({
+const log = require('hexo-log')({
   debug: false,
   silent: false
-});
+})
 
 log.info('Hello world');
 ```
@@ -27,7 +27,6 @@ Option | Description | Default
 --- | --- | ---
 `debug` | Display debug message and save log to `debug.log` file. | `false`
 `silent` | Don't display message in console. | `false`
-`name` | Name | `hexo`
 
 ## License
 
--- test/index.js
+++ test/index.js
@@ -1,12 +1,16 @@
 'use strict';
 
-const should = require('chai').should(); // eslint-disable-line
+require('chai').should();
 const rewire = require('rewire');
 const sinon = require('sinon');
 
 describe('hexo-log', () => {
   const logger = require('../lib/log');
   const loggerModule = rewire('../lib/log');
+
+  beforeEach(() => {
+    sinon.stub().reset();
+  });
 
   it('add alias for levels', () => {
     const log = logger();
@@ -16,36 +20,6 @@ describe('hexo-log', () => {
     log.w.should.eql(log.warn);
     log.e.should.eql(log.error);
     log.log.should.eql(log.info);
-  });
-
-  it('default name is hexo', () => {
-    const log = logger();
-
-    log.fields.name.should.eql('hexo');
-  });
-
-  it('options.name', () => {
-    const log = logger({ name: 'foo' });
-
-    log.fields.name.should.eql('foo');
-  });
-
-  it('level should be trace if options.debug is true', () => {
-    const log = logger({ debug: true });
-
-    log.streams[0].level.should.eql(10);
-  });
-
-  it('should add file stream if options.debug is true', () => {
-    const log = logger({ debug: true });
-
-    log.streams[1].path.should.eql('debug.log');
-  });
-
-  it('should remove console stream if options.silent is true', () => {
-    const log = logger({ silent: true });
-
-    log.streams.length.should.eql(0);
   });
 
   it('should display time if options.debug is true', () => {
@@ -66,22 +40,5 @@ describe('hexo-log', () => {
     });
 
     spy.args[0][0].should.contain(now.toISOString().substring(11, 23));
-  });
-
-  it('should print error to process.stderr stream', () => {
-    const spy = sinon.spy();
-
-    loggerModule.__with__({
-      process: {
-        stderr: {
-          write: spy
-        }
-      }
-    })(() => {
-      const log = loggerModule();
-      log.error('test');
-    });
-
-    spy.calledOnce.should.be.true;
   });
 });
